228f844b2372e41a572fe3de33d7b1134056a3f2
1FaKe110 <gabko2016@gmail.com>
2023-08-21 10:16:21 +0300

    init

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,3 @@
-
-function getCookie(name) {
-    var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-}
-
 // Если есть сохраненные данные формы, заполнить форму при загрузке страницы
 window.onload = function() {
     var form = document.querySelector('.card');
@@ -12,6 +6,11 @@ window.onload = function() {
         return
     }
     var formData = JSON.parse(formDataCookie);
+}
+
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
 function clearCookies() {
